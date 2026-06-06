2abc0f71bd94df438422edaab263e46408a18d7f
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2026-06-05 23:08:29 -0300

    add rol_id  = 9

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -68,7 +68,7 @@ import { ConfiguracionPage } from '../../modules/configuracion/pages/Configuraci
 import { GastosImprevistosPage } from '../../modules/gastosImprevistos/pages/gastosImprevistos.page';
 
 
-const ROLES_ADMIN  = [1, 3, 4, 6];
+const ROLES_ADMIN  = [1, 3, 4, 6, 9];
 const ROLES_WORKER = [7, 8];
 const ROLES_ALL    = [...ROLES_ADMIN, ...ROLES_WORKER];
 
