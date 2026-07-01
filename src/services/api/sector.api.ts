716be6ab7b1297ef147dd456ad2c8b9a1b272880
GomezPrimo-InTheHouse <juliangomezprimo@gmail.com>
2026-07-01 09:49:34 -0300

    avances en labor form 2

--- src/services/api/sector.api.ts
+++ src/services/api/sector.api.ts
@@ -2,7 +2,8 @@ import { env } from '../../app/config/env';
 import httpClient from '../httpClient';
 import type { CreateSectorPayload, Sector } from '../../modules/obras/types/sector.types';
 
-const baseUrl = env.sectoresApiUrl;
+// const baseUrl = env.sectoresApiUrl/sectores;
+const baseUrl = `${env.sectoresApiUrl}/sectores`;
 
 export const sectorApi = {
   async getByObra(obra_id: number): Promise<Sector[]> {
